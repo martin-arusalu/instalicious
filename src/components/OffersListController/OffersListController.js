d958f0f4206596857ea71f9ead88dc54b5a5396d
Martin Arusalu <martin.arusalu@gmail.com>
2018-05-16 12:01:02 +0300

    Add loading screen

--- src/components/OffersListController/OffersListController.js
+++ src/components/OffersListController/OffersListController.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import ReactLoading from 'react-loading';
 import OffersList from '../OffersList/OffersList';
 import { orderByPercentage, getPage } from '../../services/OffersListService/OffersListService';
+import './OffersListController.css';
 
 class OffersListController extends React.Component {
   constructor(props) {
     super(props);
     this.getOffers = this.getOffers.bind(this);
-    this.state = { offers: [] };
+    this.state = { loading: true, offers: [] };
+    this.PAGES_TO_RENDER = 2;
   }
 
   componentDidMount() {
@@ -18,7 +21,7 @@ class OffersListController extends React.Component {
     offers.push(...this.state.offers);
     
     const queryPages = async page => {
-      if (page < 3) {
+      if (page <= this.PAGES_TO_RENDER) {
         await getPage(page).then(json => offers.push(...json));
         await queryPages(page + 1);
       }
@@ -27,12 +30,17 @@ class OffersListController extends React.Component {
     await queryPages(1);
     offers = offers.filter(offer => parseFloat(offer.discount_percent.slice(0, offer.discount_percent.length - 1)) > 0);
     offers = orderByPercentage(offers, true);
-    this.setState({ offers });
+    this.setState({ offers, loading: false });
     console.log('offers', offers);
   }
 
   render() {
-    return <OffersList offers={this.state.offers} />
+    return (
+      <div>
+        {this.state.loading && <ReactLoading className="loadingScreen" type={'spinningBubbles'} color={'#3c0078'} />}
+        <OffersList offers={this.state.offers} />
+      </div>
+    );
   }
 }
 
